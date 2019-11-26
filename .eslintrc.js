76c9438a114d3659f357d871cf4da7e153135672
Rodrigo Gomes <rodgomesc@gmail.com>
2019-11-26 06:56:30 -0800

    breaking change - application changed to mobile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "import/prefer-default-export": "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "react/jsx-one-expression-per-line": "off",
-    "global-require": "off",
-    "no-param-reassign": "off",
-    "no-underscore-dangle": "off",
-    camelcase: "off",
-    "react/jsx-props-no-spreading": 0,
-
-    "max-len": ["error", { code: 120 }],
-    "react/prop-types": ["off"],
-    "linebreak-style": ["off"],
-    "no-console": 0,
-    "import/no-cycle": 0,
-    "react/button-has-type": ["off"],
-    "react/forbid-prop-types": ["off"],
-    "react/no-array-index-key": ["off"]
-  }
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.js']}],
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
+  },
 };
--- src/components/Navbar/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  height: 64px;
-  background: linear-gradient(90deg, #636cc3 20%, #a17ce7 100%);
-`;
