51e079f9ff1fd4a7f6a401c0b204e687b6b40942
Rodrigo Gomes <rodgomesc@gmail.com>
2019-11-27 05:21:52 -0800

    bugfix

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Overlay, HeaderText, FooterText } from './styles';
 
 export default function Card({ cardImage, ...rest }) {
+  console.log;
   return (
     <Container {...rest}>
       <Overlay source={{ uri: cardImage }}>
